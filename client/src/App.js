8fc30303edf0f87a11b5ad7afaa0ba09e480e3df
unknown <alexandrannance@gmail.com>
2021-01-28 10:20:19 -0600

    import store and provider into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,20 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
-import { StoreProvider } from "./utils/GlobalState";
 import Success from "./pages/Success";
 import OrderHistory from "./pages/OrderHistory";
+
+// Remove StoreProvider since we will be using store and Provider from react-redux
+// import { StoreProvider } from "./utils/GlobalState";
+import store from "./utils/store";
+import { Provider } from "react-redux";
+
 
 const client = new ApolloClient({
   request: (operation) => {
@@ -30,7 +34,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -41,11 +45,10 @@ function App() {
               <Route exact path="/products/:id" component={Detail} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
-
   );
 }
 
